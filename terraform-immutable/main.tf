8bfaa5a3b6fc1f06bd5b694f9767446766eadf6e
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-19 06:49:11 +0530

    Update roboshop-components/cart/

--- terraform-immutable/main.tf
+++ terraform-immutable/main.tf
@@ -1,12 +1,12 @@
 module "cart" {
-  source         = "./vendor/modules/app"
-  ENV            = var.ENV
-  INSTANCE_TYPE  = var.INSTANCE_TYPE
-  COMPONENT      = var.COMPONENT
-  APP_PORT       = var.APP_PORT
-  WORKSTATION_IP = var.WORKSTATION_IP
-  APP_VERSION    = var.APP_VERSION
-  //LB_TYPE             = "internal"
+  source           = "./vendor/modules/app"
+  ENV              = var.ENV
+  INSTANCE_TYPE    = var.INSTANCE_TYPE
+  COMPONENT        = var.COMPONENT
+  APP_PORT         = var.APP_PORT
+  WORKSTATION_IP   = var.WORKSTATION_IP
+  APP_VERSION      = var.APP_VERSION
+  LB_TYPE          = "internal"
   APP_TYPE         = var.APP_TYPE
   PROMETHEUS_IP    = var.PROMETHEUS_IP
   DESIRED_CAPACITY = var.DESIRED_CAPACITY
